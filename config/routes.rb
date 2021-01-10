332500f0ad14cbe59680d6fe3391a9f569b036d3
nemotomasafuminoMacBook-Pro <auther.masahito@gmail.com>
2021-01-10 15:44:43 +0900

    　ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get  'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
+  $date = Time.now.in_time_zone('Tokyo').to_s
+  root "articles#index"
+  resources :articles, only: :index
 end
